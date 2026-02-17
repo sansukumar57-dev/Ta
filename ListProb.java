b571bf0dfa4064264c7bc5c66820402c639d59cb
sansukumar57-dev <sansukumar57@gmail.com>
2026-02-17 18:51:25 +0530

    Add files via upload

--- ListProb.java
+++ ListProb.java
@@ -24,6 +24,38 @@ public class ListProb {
        System.out.println(list.get(1));
        list.remove("zoro");
        System.out.println("After deleted : "+list);
+       ArrayList<String> tasks= new ArrayList<>();
+       tasks.add("Study Dsa");
+       tasks.add("Build Projects");
+       tasks.add("Study Dsa");// duplicates can be allowed
+       System.out.println(tasks);
+       //add
+       tasks.add("New Tasks");
+       tasks.remove(2);
+       tasks.get(0);
+       tasks.size();
+       tasks.contains("Studey");
+       HashSet<String> set = new HashSet<>();
+
+       set.add("Java");
+       set.add("Python");
+       set.add("Java"); // ignored
+
+       System.out.println(set);
+       Queue<String> queue = new LinkedList<>();
+
+       queue.add("Task 1");
+       queue.add("Task 2");
+
+       System.out.println(queue.poll()); // removes first
+       HashMap<Integer, String> students = new HashMap<>();
+
+       students.put(1, "Arun");
+       students.put(2, "Vijay");
+
+       System.out.println(students.get(1));
+
+
 
    }
 }
