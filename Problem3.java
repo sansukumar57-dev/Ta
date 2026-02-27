0b9fd813f252ef3e4ebd7d81cfaaf75e055cfc77
sansukumar57-dev <sansukumar57@gmail.com>
2026-02-27 18:10:51 +0530

    Add files via upload

---
+++ Problem3.java
@@ -0,0 +1,18 @@
+package ProblemList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        List<Integer> ls=new ArrayList<>();
+        ls.add(2);
+        ls.add(1);
+        ls.add(3);
+        System.out.println(ls);
+        System.out.println(ls.size());
+        System.out.println(        ls.get(2));
+        System.out.println(ls.isEmpty());
+
+    }
+}
