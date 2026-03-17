d0175a2bec0e6c9fb2d9575d7277bf4d909731cd
sansukumar57-dev <sansukumar57@gmail.com>
2026-03-17 18:43:32 +0530

    Add files via upload

--- Prob4.java
+++ Prob4.java
@@ -1,28 +1,61 @@
-package Problems;
-
-import java.util.Scanner;
-
+package Prb;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 public class Prob4 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        int prev=0;
-        int dub=0;
-        for(int i=0;i< arr.length;i++){
-            if( arr[i]>prev){
-                prev=arr[i];
-                dub=1;
-            }
-            if(prev == arr[i]){
-               dub++;
-            }
 
-        }
-        System.out.println(dub);
+            public static void main(String[] args) throws IOException {
+                Scanner sc=new Scanner(System.in);
+                int n=sc.nextInt();int[] arr=new int[n];
+                int k=sc.nextInt();
+                for(int i=0;i<n;i++){
+                    arr[i]=sc.nextInt();
+                }
+                System.out.println(Result.divisibleSumPairs(n, k, arr));
 
+            }
+        }
+
+
+class Result {
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, int[] ar) {int c=0;
+        // Write your code here
+        for(int i=0;i<ar.length;i++){
+            for(int j=0;j<ar.length;j++){
+                if(i<j){
+
+                    if((ar[i]+ar[j])%k==0){
+                        c++;
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+        return c;
     }
+
 }
