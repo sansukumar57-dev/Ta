71f6cbdf6ab8bbf8698128b6d9bd47b8778da808
sansukumar57-dev <sansukumar57@gmail.com>
2026-03-20 10:52:44 +0530

    Add files via upload

---
+++ Prob7.java
@@ -0,0 +1,14 @@
+package Prb;
+
+public class Prob7 {
+    public static void main(String[] args) {
+        int n=123;
+int c=0;
+       while(n>0){
+          n= n/10;
+          c++;
+            }
+
+        System.out.println(c);
+    }
+}
